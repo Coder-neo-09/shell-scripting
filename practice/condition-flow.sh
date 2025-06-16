9106d27ed5b112ae9fa8d7304b795a9b85fdf030
Coder-neo-09 <neocoder09@gmail.com>
2025-06-16 21:51:04 +0530

    Update condition-flow.sh

---
+++ practice/condition-flow.sh
@@ -0,0 +1,40 @@
+#! /usr/bin/bash
+
+echo "Enter username: "
+read username
+
+read -p "Enter age: " age
+
+echo "Hey $username, you are $age old"
+
+if [ $username = "admin" ]; then
+    echo "Welcome, admin!"
+else
+    echo "Hello, $username"
+fi
+
+if [ $age -lt 13 ]; then
+    echo "Access denied"
+elif [ $age -lt 18 ]; then
+    echo "Limited Access"
+else
+    echo "Full Access"
+fi
+
+# read -p "Which team you belong to : " team
+echo "Which team you belong to : "
+echo "ux"
+echo "frontend"
+echo "backend"
+read team
+
+case $team in
+	ux)
+		echo "Please start working design login page";;
+	frontend)
+		echo "Develop UI component for login page";;
+	backend)
+		echo "Implement authentication api for login page";;
+	*)
+		echo "Sorry not allowed to work";;
+esac
