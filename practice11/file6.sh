7ee5888e04506a6b9eaa3caaab59ed30efdc2333
Coder-neo-09 <neocoder09@gmail.com>
2025-07-10 13:57:40 +0000

    files added

--- practice11/file6.sh
+++ practice11/file6.sh
@@ -1,10 +1,10 @@
-#!  /usr/bin/bash
+#! /usr/bin/bash
 
 sites=("google.com" "github.com" "fake.site")
 
 for site in "${sites[@]}"
 do
-  if ping -c 1 $site &> /dev/null
+  if ping -c 1 $site &> /dev/null:
   then
     echo "$site is reachable."
   else
